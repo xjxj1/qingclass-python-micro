dad7b509726c63ebab1d789facb5cc820281b2d2
王晓龙 <wangxiaolong@qingclass.com>
2018-12-11 15:25:31 +0800

    增加docker-compose管理并热加载代码

--- project/__init__.py
+++ project/__init__.py
@@ -3,12 +3,14 @@
 # @Author  : Wang XiaoLong (xiaolongw123@gmail.com)
 # @Link    : https://github.com/xjxj1
 # @Version : $Id$
+import os
 from flask import Flask, jsonify
 
 # 初始化app
 app = Flask(__name__)
 # 环境配置
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/ping', methods=['get'])
 def ping_pong():
@@ -16,4 +18,3 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
         })
-
--- project/config.py
+++ project/config.py
@@ -3,25 +3,27 @@
 # @Author  : Wang XiaoLong (xiaolongw123@gmail.com)
 # @Link    : https://github.com/xjxj1
 # @Version : 1.0
+import os
 class BaseConfig:
     """ 基础配置 """
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig:
     """ 开发环境配置 """
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig:
     """ 测试环境配置 """
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig:
     """ 生产环境配置 """
     DEBUG = False
-
-
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
